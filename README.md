50bbff1120af3064e8628e75a0dbccae5e698f5f
Arianne Bortolan <arianne.bortolan@bringit.com.br>
2019-02-05 09:27:38 -0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # better-life-scripts
 Simple Bash scripts to make anyones life easy
+Scripts Use: 
+- create.sh: created a user in a machine, option of creating with sudo and ssh access. 
---
+++ create.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#user creation script
+
+#username of the user to be created
+user1=username
+
+#if the user will have a ssh key, key = 1, ssh var has the user key
+key=0
+ssh=0
+#if the user will have sudo access or not
+sudoer=0
+
+#user1 creation
+useradd -s /bin/bash -m $user1
+
+if [ $ssh = 1 ]; then
+    mkdir /home/$user1/.ssh
+    touch /home/$user1/.ssh/authorized_keys
+    echo $key > /home/$user1/.ssh/authorized_keys
+    chmod 700 /home/$user1/.ssh/
+    chmod 640 /home/$user1/.ssh/authorized_keys
+    chown -R $user1:$user1 /home/$user1/.ssh/
+fi
+
+if [ $sudoer = 1 ]; then
+   usermod -aG sudo $user1
+   echo "user with sudo access created"
+else 
+   echo "user created"
+fi
+
